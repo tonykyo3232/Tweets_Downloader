1c54948101f28068caae4df140d36f4b390cc76b
tonykyo3232 <tonykyo3232@gmail.com>
2021-10-25 22:17:37 -0700

    current version

---
+++ main.py
@@ -0,0 +1,26 @@
+from twitter_client import TwitterClient
+from tweet_analyzer import TweetAnalyzer
+
+def get_twitter_feed():
+
+    twitter_client = TwitterClient()
+    tweet_analyzer = TweetAnalyzer()
+
+    api = twitter_client.get_twitter_client_api()
+
+    x = 'jimcramer'
+    y = 200
+    
+    # twitter api only allows us to download 200 tweets at one time
+    tweets = api.user_timeline(screen_name=x, count=y, tweet_mode='extended')
+
+    tweet_analyzer.set_account(x)
+
+    tweet_analyzer.set_num_of_tweets(y)
+
+    df = tweet_analyzer.tweets_to_data_frame3(tweets)
+
+    # upload the file to firebase
+    tweet_analyzer.upload_fire_base(df)
+
+get_twitter_feed()
---
+++ popular_twitter_accounts.py
@@ -0,0 +1,14 @@
+'''
+returns a dictionary of twitter account that discusses stock market
+# reference: https://mywallst.com/blog/best-investing-twitter-accounts/
+'''
+
+accounts = {
+	'Financial Times': 'FinancialTimes',
+	'Peter Brandt': 'PeterLBrandt',
+	'Investors.com': 'IBDinvestors',
+	'LizAnnSonders': 'Liz Ann Sonders',
+	'Charlie Bilello': 'charliebilello',
+	'Earnings Whispers': 'eWhispers',
+	'Jim Cramer': 'jimcramer'
+	}
